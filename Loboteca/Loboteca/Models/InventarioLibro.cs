5780a58c36b18471109d93763097e660bb3121e8
SamSanchez710 <samaragomez855@gmail.com>
2024-11-27 12:05:27 -0600

    boton inventario

--- Loboteca/Loboteca/Models/InventarioLibro.cs
+++ Loboteca/Loboteca/Models/InventarioLibro.cs
@@ -1,15 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Loboteca.Models
 {
     public partial class InventarioLibro
     {
         public int Id { get; set; }
+
+        [Display(Name = "Número de Inventario")]
         public int? IdInventario { get; set; }
+
+        [Display(Name = "Libro")]
         public int? IdLibro { get; set; }
+
+        [Display(Name = "Fecha de Apertura")]
         public DateTime FechaApertura { get; set; }
+
+        [Display(Name = "Fecha de Cierre")]
         public DateTime FechaCierre { get; set; }
+
+        [Display(Name = "Tipo de Inventario")]
         public string InventarioTipo { get; set; } = null!;
 
         public virtual Inventario? IdInventarioNavigation { get; set; }
